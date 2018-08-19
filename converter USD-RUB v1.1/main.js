1cb38c1f36c23cb88982cb3a65e2984085bf8f12
Alex <golovchenko.aleksey@ro.ru>
2018-08-19 16:35:31 +0300

    add description, correct version

--- converter USD-RUB v1.1/main.js
+++ converter USD-RUB v1.1/main.js
@@ -1,22 +1,23 @@
-
 // event after click enter
 function calc() {
   //get answer span
   let an = document.getElementById("an");
-  // get input value
+  // get inputs value
   let rubCost = document.getElementById("rubCost").value;
   let dollarCost = document.getElementById("dollarCost").value;
   let dollarValue = document.getElementById("dollarValue").value;
   let rubValue = document.getElementById("rubValue").value;
   let re = /^[\d]*[,.]?[\d]+$/; //match only numbers
   let tempDol, tempRub, res; //temp and main result
-  // cost must be
+  // cost and one of value must exist. also they cant be equal null
   if (rubCost && dollarCost && dollarValue && rubCost != 0 && dollarCost != 0 && dollarValue != 0 || rubCost && dollarCost && rubValue && rubCost != 0 && dollarCost != 0 && rubValue != 0) {
+    // check is numbers
     if (!re.test(rubCost) || !re.test(dollarCost)) {
       res = "You didnt write a number or perhaps you wrote a negative number. Please, try again!";
       an.innerHTML = res;
-      return;
+      return; //stop function execution
     }
+    // check is number if exist
     if (dollarValue) {
       if (!re.test(dollarValue)) {
         res = "You didnt write a number or perhaps you wrote a negative number. Please, try again!";
@@ -24,6 +25,7 @@ function calc() {
         return;
       }
     }
+    // check is number if exist
     if (rubValue) {
       if (!re.test(rubValue)) {
         res = "You didnt write a number or perhaps you wrote a negative number. Please, try again!";
@@ -37,7 +39,7 @@ function calc() {
     return;
   }
   let reComma = /,/; // match a single comma
-  // check a single comma and replace by dot
+  // check is a single comma exists and replace by dot
   if (reComma.test(rubCost)) {
     rubCost = rubCost.replace(/,/, '.');
   };
@@ -50,14 +52,17 @@ function calc() {
   if (reComma.test(rubValue)) {
     rubValue = rubValue.replace(/,/, '.');
   };
+  // calc values
   if (dollarValue) {
     tempRub = (rubCost * dollarValue / dollarCost).toFixed(2);
     tempRub = parseFloat(tempRub);
   }
+  // calc values
   if (rubValue) {
     tempDol = (rubValue * dollarCost / rubCost).toFixed(2);
     tempDol = parseFloat(tempDol);
   }
+  // combine result
   if (tempDol && tempRub) {
     res = `${tempRub} rub  /  ${tempDol} doll`;
   } else if (tempRub) {
@@ -65,9 +70,11 @@ function calc() {
   } else if (tempDol) {
     res = `${tempDol} doll`;
   }
+  // print reault inside span
   an.innerHTML = res;
 };
 
+// execute function after click Enter
 document.onkeyup = function(e) {
   // e = e || window.event;
   if (e.keyCode === 13) {
