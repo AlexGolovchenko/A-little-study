35aec189eec1ddde4465bfb6d08720da820904c4
Alex <golovchenko.aleksey@ro.ru>
2018-08-23 14:12:29 +0300

    completed

--- some tasks/voximplant/sum task.js
+++ some tasks/voximplant/sum task.js
@@ -5,43 +5,41 @@
 
 VoxEngine.addEventListener(AppEvents.CallAlerting, e => {
   const call_customer = e.call;
-  let result = 0;
-  let arr_nums = [];
   // answer the customer
   call_customer.answer();
+  //let customer to enter tone
   call_customer.handleTones(true);
-  // say hello to customer when connected
+  //execute function when connected
   call_customer.addEventListener(CallEvents.Connected, e => {
+    //say hi and task to customer
     call_customer.say(`hello, my name is Alex. Please, enter a number!`, Language.US_ENGLISH_FEMALE);
-    call_customer.addEventListener(CallEvents.ToneReceived, e => {
-                                          //EROR
-                                          arr_nums.push(call_customer.customData());
-                                          //не происходит передача данных от пользователя в e.call.customData().
-                                          //Вопрос: а должна ли происходить через этот метод? Если да, то как.
-                                          //Если нет, то как передать данные переменной то, что ввел пользователь?
-                                          //Вопрос2: как дебажить код в этой среде? пробовал через Отладку, не сообразил в чем соль.. логи помогали отчасти
-                                          //так может и разобрался бы "методом тыка" через консоль)
-      									  //остальное должно работать: робот общается, логи в норме, код проверял в Node
-      call_customer.say(`All right! Please, enter a number again`, Language.US_ENGLISH_FEMALE);
+    call_customer.addEventListener(CallEvents.PlaybackFinished, e => {
       call_customer.addEventListener(CallEvents.ToneReceived, e => {
-                                          //EROR
-                                          arr_nums.push(call_customer.customData());
-                                          //
-        for (let i = 0; i < arr_nums.length; i++) {
-          arr_nums[i] = +arr_nums[i]
-        }
-        result = arr_nums.reduce((sum, item) => sum + item, 0);
-        let x = true;
-        if (x) {
-          call_customer.say(result.toString(10), Language.US_ENGLISH_FEMALE)
-        }
+        //get first value
+        let first = Number(e.tone);
+        //say next task to customer
+        call_customer.say(`All right! Please, enter a number again`, Language.US_ENGLISH_FEMALE);
+        call_customer.addEventListener(CallEvents.PlaybackFinished, e => {
+          call_customer.addEventListener(CallEvents.ToneReceived, e => {
+            //get second value
+            let second = Number(e.tone);
+            //switch true if ready to tell
+            let x = true;
+            if (x) {
+              //say sum values to customer
+              call_customer.say((first + second).toString(), Language.US_ENGLISH_FEMALE)
+            }
+          });
+        });
       });
     });
   });
+  //execute function when failed
   call_customer.addEventListener(CallEvents.Failed, e => {
     VoxEngine.terminate();
   });
+  //execute function when disconnected
   call_customer.addEventListener(CallEvents.Disconnected, e => {
-    VoxEngine.terminate()
+    VoxEngine.terminate();
   });
 });
